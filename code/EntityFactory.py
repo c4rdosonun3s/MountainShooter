b7ef24cffbe3ce627cebf9a9b482da90bd9458d6
CardosoNunes <matheusnunes834@gmail.com>
2025-03-26 00:16:11 -0300

    Creating player and enemy

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Background import Background
-from Const import WIN_WIDTH
+import random
 
+from code.Background import Background
+from Const import WIN_WIDTH, WIN_HEIGHT
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -16,4 +19,12 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0,0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player ('Player1', (10, WIN_HEIGHT / 2 - 30))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1',(WIN_WIDTH + 10, random.randint(30, WIN_HEIGHT - 30)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(0, WIN_HEIGHT - 30)))
 
